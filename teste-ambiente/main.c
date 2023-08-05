873a6bfa2477fd82b51bb97bd49e46f884b3c2c0
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2023-08-05 10:36:51 -0300

    logica primos
    Logica de verificacao de numeros Primos

--- teste-ambiente/main.c
+++ teste-ambiente/main.c
@@ -1,7 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+//logica do profesor
+
+int verificaNumeroPrimo(int n){
+    int ret = 0;
+    if (n % 2 == 0){
+        return = 0;
+    }
+while (cont < n){
+    if(n % cont==0){
+        return 0;
+    }   
+    cont += 2;
+    return 1;
+    }
+}
+
+
+//logica do GPT
+bool ehPrimo(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    if (n % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 
 int main(){
-    printf("Hello World")
+    int n = 123131231;
+    printf("n = %d  \n", n);
+    printf("O numero %d eh primo? %d\n", n, ehPrimo(n));
+
+
     return 0;
+
+
+
 }
+
