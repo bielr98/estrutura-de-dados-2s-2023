ef81e677c9cdd0af67041b59394e8acaaa4abe37
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2023-08-12 10:09:37 -0300

    Arquivos criados
    Arquivps de teste criados

---
+++ teste/teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (int argc, char* argv[]){
+    printf("Hello World");
+    printf( "%s", argv[0]);
+
+    return 0;
+}
+
+
+//compilar gcc nomedoarquivo.c -o nomeQueVoceQuerNoArquivoExecutavel
